a5292d38b1784ac64e8fb110eb29b59d7b6907b8
baddeverVN <minhtien1107vn@gmail.com>
2016-02-26 19:00:11 +0700

    mmt

---
+++ app/src/main/java/com/example/minhtien/demolistactivity/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.minhtien.demolistactivity;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends ListActivity {
+    private TextView text;
+    private List<String> listValues;
+    private String str = "number";
+    private int number  = 0;
+    private ArrayAdapter<String> myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.mainText);
+
+        listValues = new ArrayList<String>();
+        listValues.add("Android");
+        listValues.add("iOS");
+        listValues.add("Symbian");
+        listValues.add("Blackberry");
+        listValues.add("Windows Phone");
+
+        // initiate the listadapter
+       myAdapter = new ArrayAdapter <String>(this,
+                R.layout.row_layout, R.id.listText, listValues);
+
+        // assign the list adapter
+        setListAdapter(myAdapter);
+
+    }
+
+    // when an item of the list is clicked
+    @Override
+    protected void onListItemClick(ListView list, View view, int position, long id) {
+        super.onListItemClick(list, view, position, id);
+
+        String selectedItem = (String) getListView().getItemAtPosition(position);
+        //String selectedItem = (String) getListAdapter().getItem(position);
+
+        text.setText("You clicked " + selectedItem + " at position " + position);
+        number++;
+        listValues.add(str + Integer.toString(number));
+        myAdapter.notifyDataSetChanged();
+    }
+}
